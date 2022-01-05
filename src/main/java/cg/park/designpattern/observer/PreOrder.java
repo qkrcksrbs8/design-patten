acb58814f77a8ff7bae8b255fcf025a7cb5dcd35
changyun <qkrcksrbs8@naver.com>
2022-01-05 19:29:35 +0900

    observer pattern - main class에 주석 추가

--- src/main/java/cg/park/designpattern/observer/PreOrder.java
+++ src/main/java/cg/park/designpattern/observer/PreOrder.java
@@ -10,11 +10,11 @@ public class PreOrder {
         VegetableShop vegetableShop = new VegetableShop(preorderData);
         FoodShop foodShop           = new FoodShop(preorderData);
 
-        preorderData.setFoodMaterials(10, 20, 30);
-        preorderData.setFoodMaterials(5, 20, 30);
+        preorderData.setFoodMaterials(10, 20, 30);//오픈 시 재고
+        preorderData.setFoodMaterials(5, 20, 30);//고기 5개 판매
 
-        preorderData.removeObserver(vegetableShop);
+        preorderData.removeObserver(vegetableShop);//야채가게는 별도로 창고를 구입하여 이제 연락받지 않음.
 
-        preorderData.setFoodMaterials(5, 10, 30);
+        preorderData.setFoodMaterials(5, 10, 30);//과일 10개 판매
     }
 }
